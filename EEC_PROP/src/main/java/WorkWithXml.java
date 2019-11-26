1083ae01d9a463f69c19e7205bdd58827ffa7bbe
Kokin Andrey <akokin-cbgr@users.noreply.github.com>
2019-11-26 17:28:38 +0300

    заработало MQ

---
+++ EEC_PROP/src/main/java/WorkWithXml.java
@@ -0,0 +1,39 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WorkWithXml {
+  public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+   /* try(FileReader reader = new FileReader("D://MSG_2.xml"))
+    {
+      // читаем посимвольно
+      int c;
+      while((c=reader.read())!=-1){
+
+        System.out.print((char)c);
+      }
+    }
+    catch(IOException ex){
+
+      System.out.println(ex.getMessage());
+    }*/
+    // Создается построитель документа
+    //DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+    // Создается дерево DOM документа из файла
+    //Document document = documentBuilder.parse("D://MSG_2.xml");
+    //Node root = document.getDocumentElement();
+    //NodeList books = root.getChildNodes();
+    //System.out.println(books.item(1) + books.item(1).getNodeName() +  " : " + books.item(1).getTextContent());
+
+    File file = new File("D://MSG_2.xml");
+    BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+    System.out.println(bufferedReader.toString().toCharArray());
+    bufferedReader.close();
+
+  }
+
+}
---
+++ EEC_PROP/src/test/java/WorkWithXmlTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+import java.util.NoSuchElementException;
+
+@Test
+public class WorkWithXmlTest extends WorkWithXml {
+  public void test() throws NoSuchElementException {
+  /*  try {
+      XMLParser xmlParser = new
+      JSONObject jsonObject = new JSONObject(request("eek-test1prop-mg2.tengry.com:27017", "service-prop-65", "radioElectronicDeviceRegistryDetailsType",
+              "propositionInclusionRadioElectronicDeviceId","PBY00000000000000204"));
+      Assert.assertTrue(jsonObject.get("statusCode").toString().contains("002"));
+    }
+    catch (NoSuchElementException e){
+      System.out.println(e);
+    }*/
+  }
+}
